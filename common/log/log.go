f0965fe7b153576db35e87965c0fefd5102b49eb
l_yunwen@qq.com <l_yunwen@qq.com>
2019-04-11 14:52:05 +0800

    ~

--- common/log/log.go
+++ common/log/log.go
@@ -13,7 +13,7 @@ func Default(log string) {
 }
 func Warn(log string) {
 	log = "\r\n" + time.Now().Format("2006-01-02 15:04:05") + "|" + log
-	f, _ := os.OpenFile("log/Warn.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	f, _ := os.OpenFile("log/warn.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	_, _ = f.WriteString(log)
 }
 
